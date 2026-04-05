d41d85c2b1965a57b7b971c70e0c36e1ed45a86e
Real <53586056+RealEawo5@users.noreply.github.com>
2026-04-05 22:52:35 +0200

    refactor: added comment to match previous commit

--- parse.py
+++ parse.py
@@ -83,7 +83,7 @@ class ABMFile:
 
         
         # Check if the pixel data is shorter than expected
-        if len(self.__rawPixelData) < expectedSize:
+        if len(self.__rawPixelData) < expectedSize:                                     # ? We use the raw pixel data length here to check for padding, since the formatted pixel data may be shorter due to format-specific processing (e.g. removing alpha channel)
             print(f" Warning! Expected pixel data size {expectedSize} bytes, but got {len(self.pixelData)} bytes. The image may be incomplete or corrupted.")
 
             # Pad with zeros
